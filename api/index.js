f7c46fa0d5c32b4511785a0786c8917c87d85cd4
yan-dashuai <2220867980@qq.com>
2025-10-23 10:49:23 +0800

    完成

---
+++ api/index.js
@@ -0,0 +1,7 @@
+// Next.js API route
+// This file serves as a simple API endpoint
+
+export default function handler(req, res) {
+  // Return a simple response
+  res.status(200).json({ message: 'API endpoint is working' });
+}
